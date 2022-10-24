b0a283854d74662232146cb578e7994c3d8e9fa7
Hossein Davoodi <h.davoodi@trenkwalder.com>
2022-10-24 09:29:13 +0300

    added logic of progressBar and animation

--- src/component/ProgressBar.tsx
+++ src/component/ProgressBar.tsx
@@ -20,21 +20,51 @@ const ProgressBar = ({
     { id: 3, item: cv, text: "CV and / or other documents" },
     { id: 4, item: profileImg, text: "Profile image" },
   ];
+  const count = progressBarItems.filter((value) => value.item).length;
 
   const { barText, barWidth, bgColor } = useMemo(() => {
     let bgColor;
     let barWidth;
     let barText;
 
+    switch (count) {
+      case 0:
+        bgColor = "bg-neutral-400";
+        barWidth = "w-6";
+        barText = "none";
+        break;
+      case 1:
+        bgColor = "bg-red-500";
+        barWidth = "w-1/4";
+        barText = "basic info";
+        break;
+      case 2:
+        bgColor = "bg-orange-500";
+        barWidth = "w-1/2";
+        barText = "mid info";
+        break;
+      case 3:
+        bgColor = "bg-lime-500";
+        barWidth = "w-3/4";
+        barText = "good info";
+        break;
+      case 4:
+        bgColor = "bg-green-500";
+        barWidth = "w-full";
+        barText = "all star info";
+        break;
+    }
     return { barText, barWidth, bgColor };
-  }, []);
+  }, [count]);
 
   return (
     <>
       <p className="p_highlight mb-1">Profile Progress</p>
 
       <div className="w-full bg-slate-200 rounded-full h-6">
-        <div className={`h-full rounded-full ${barWidth} ${bgColor}`}></div>
+        <div
+          className={`h-full rounded-full transition-width duration-500 ease-in-out ${barWidth} ${bgColor}`}
+        ></div>
       </div>
       <p className="micro_highlight text-text-secondary mt-1 mb-12">
         {barText}
